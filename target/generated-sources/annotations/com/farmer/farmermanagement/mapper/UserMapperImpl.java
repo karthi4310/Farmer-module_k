73963602ddf83fb6f510ba1d0e2aa43a43a1b17c
karthi4310 <karthik.m@hinfinity.in>
2025-06-17 09:11:56 +0530

    registration done

--- target/generated-sources/annotations/com/farmer/farmermanagement/mapper/UserMapperImpl.java
+++ target/generated-sources/annotations/com/farmer/farmermanagement/mapper/UserMapperImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-06-13T08:23:35+0530",
+    date = "2025-06-17T09:11:20+0530",
     comments = "version: 1.5.5.Final, compiler: Eclipse JDT (IDE) 3.42.0.v20250514-1000, environment: Java 21.0.7 (Eclipse Adoptium)"
 )
 @Component
